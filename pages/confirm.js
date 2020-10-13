e4b4a976ceb29d65f0c0f8793ecd374e1d2f3125
Juan Gomes Macário <jgomesmacario@gmail.com>
2020-10-13 18:52:34 -0300

    creditos

--- pages/confirm.js
+++ pages/confirm.js
@@ -1,6 +1,8 @@
 const Confirm = () => (
-  <div className="container mx-auto">
-    Confirmações
+  <div className="container mx-auto text-white">
+    <h1>Steam App Redesign Concept</h1>
+    <img src="https://cdn.dribbble.com/users/994854/avatars/normal/963448469602c4cedf74ca72d4d32aec.png?1590442499" className="w-20 h-20 rounded-full shadow-sm"/>
+    <span>by <a href="https://dribbble.com/kamilkoziel" className="text-red-400 font-medium">Kamil Kozieł</a> for <a href="https://dribbble.com/itmagination" className="text-red-400 font-medium">ITMAGINATION</a></span>
   </div>
 );
 
